7680a2db1195d4bf3900c0709c6e613eadb9d8c6
Alexander <xanderx2201@yandex.ru>
2022-08-31 23:27:51 +0300

    DZ_2

---
+++ DZ_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+Console.WriteLine("Введите первое число");
+
+string input1 = Console.ReadLine();
+
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Введите второе число");
+
+string input2 = Console.ReadLine();
+
+int number2 = int.Parse(input2);
+
+Console.WriteLine("Введите третье число");
+
+string input3 = Console.ReadLine();
+
+int number3 = int.Parse(input3);
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.Write("MAX->");
+        Console.Write(number1);
+    }
+
+    else 
+    {
+        Console.Write("MAX->");
+        Console.Write(number3);
+
+    }
+}
+else if (number2 > number3)
+    {
+        Console.Write("MAX->");
+        Console.Write(number2);
+    }
+else
+{
+    Console.Write("MAX->");
+    Console.Write(number3);
+}
