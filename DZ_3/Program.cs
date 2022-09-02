c7f0e2cf111b7bd95a57e241eebb619c8536c9bd
Alexander <xanderx2201@yandex.ru>
2022-09-02 19:53:27 +0300

    DZ-3

---
+++ DZ_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Введите число");
+
+string input1 = Console.ReadLine();
+
+int number1 = int.Parse(input1);
+
+int ostatok = number1 % 2;
+
+if (ostatok > 0)
+    Console.Write("Это нечётное число");
+else
+    Console.Write("Это чётное число");
