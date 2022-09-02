b0f1a22b1b5950c74048c67dc7dbac6a1dfc6a74
Alexander <xanderx2201@yandex.ru>
2022-09-02 22:10:30 +0300

    DZ_4

---
+++ DZ_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Введите число от 2 до бесконечности");
+
+string input1 = Console.ReadLine();
+int number = int.Parse(input1);
+
+if (number < 2)
+    Console.Write("Неправильное число");
+else
+    for (int i = 2; i <= number; i = i + 2)
+        Console.Write($"{i},");
